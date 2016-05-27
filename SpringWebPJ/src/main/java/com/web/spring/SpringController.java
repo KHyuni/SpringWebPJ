9e534b5ccc1d9c8660c4b197db5aab9e808861a1
KHyuni <hyuni0207@gmail.com>
2016-05-27 21:49:55 +0900

    스프링 4.0

---
+++ SpringWebPJ/src/main/java/com/web/spring/SpringController.java
@@ -0,0 +1,15 @@
+package com.web.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+public class SpringController {
+	
+	@RequestMapping(value = "/")
+	public String index() {
+
+		return "/index";
+	}
+	
+}
---
+++ SpringWebPJ/src/main/java/com/web/spring/vo/SpringVo.java
@@ -0,0 +1,35 @@
+package com.web.spring.vo;
+
+public class SpringVo {
+
+	private int userNo;
+	private String userId;
+	private String userPwd;
+	private String userName;
+	 
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+}
