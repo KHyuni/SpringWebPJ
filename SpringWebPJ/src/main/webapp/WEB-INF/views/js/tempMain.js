830544960b8dfb583125a9becae733663538782d
KHyuni <hyuni0207@gmail.com>
2016-05-27 21:43:52 +0900

    Revert "Spring 4.0"
    This reverts commit 35adf87d358ca96ac625f518fc3af17b157edae5.

--- SpringWebPJ/src/main/webapp/WEB-INF/views/js/tempMain.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){ 
-    $("ul#navigation li a").click(function() {
-		$("ul#navigation li").removeClass("selected");
-		$(this).parents().addClass("selected");
-		return false;
-	});
-});
